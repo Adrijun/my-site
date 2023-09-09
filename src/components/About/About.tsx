26ab43b3b466b3e7d91a99e60d035d2cc77408eb
Jonas <jonasericsson90@hotmail.com>
2023-09-10 01:51:12 +0200

    Removed pdf files and added new text in About , added stack icons

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import profileImage from '../../assets/images/profil1.bw1.jpg';
-import downLoadIcon from '../../assets/icons/download.svg';
-import { useState } from 'react';
-import { Document, Page } from 'react-pdf';
-// import CV from '../../../public/CV.pdf';
+import '../../scss/About.scss';
+import TechStack from '../../utils/techStack';
+
 function About() {
-  const [numPages, setNumPages] = useState<number>();
-  const [pageNumber, setPageNumber] = useState<number>(1);
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    setNumPages(numPages);
-  }
   return (
     <>
-      <Container className="mt-2 mb-3">
+      <Container className="mt-2 mb-3 ">
         <Row>
           <Col md={12} lg={12} className="anonymous-portfolio ">
             <Container className="box shadow rounded p-2 mb-1 project-wrapper ">
@@ -28,53 +22,58 @@ function About() {
                     />
                   </div>
                 </Col>
-                <Col md={6} lg={8}>
-                  <div className="projectsContent">
+                <Col md={6} lg={8} className="">
+                  <article>
                     <p>
-                      You can download my CV for more detailed information about
-                      my experiences and skills. If you have any questions or if
-                      you'd like to discuss how I can support your organization,
-                      I'd be more than happy to hear from you.
+                      My name is Jonas and I live in Stockholm, Sweden with my
+                      family. Although I am new to Front-end, I have a passion
+                      for development and like the small details, both design
+                      and semantically written code is something I value highly.
                     </p>
-                    <div className="button-group">
-                      <a
-                        href="/public/CV.pdf"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        type="application/pdf"
-                        download="CV.pdf"
-                        className="btn btn-outline-light m-1 mt-3"
-                      >
-                        <Image
-                          src={downLoadIcon}
-                          style={{ marginRight: '10px' }}
-                        />
-                        CV (Swe)
-                      </a>
-                      <a
-                        href="/assets/documents/CV(eng).pdf"
-                        download="CV(eng).pdf"
-                        className="btn btn-outline-light m-1 mt-3"
-                      >
-                        <Image
-                          src={downLoadIcon}
-                          style={{ marginRight: '10px' }}
-                        />
-                        CV (Eng)
-                      </a>
-                    </div>
-                  </div>
-                  {/* <div>
-                    <Document
-                      file="CV.pdf"
-                      onLoadSuccess={onDocumentLoadSuccess}
-                    >
-                      <Page pageNumber={pageNumber} />
-                    </Document>
+                  </article>
+                  <article>
+                    <h2>Education</h2>
                     <p>
-                      Page {pageNumber} of {numPages}
+                      I graduated in Frontend development in June 2023 at the
+                      Mediainstitutet in Stockholm where I have learned mostly
+                      how to work in React but also some in Angular. I also got
+                      to try how it is to work backend with Node.js. During
+                      school I had an internship at a startup company called
+                      Buddywise. Their idea is to prevent workplace accidents
+                      using AI and cameras. In the first part of my internship I
+                      got to help finalise some parts of their user interface.
+                      Here I learnt how to work with agile working methods and
+                      work with Bootstrap. In addition to my everyday tasks, I
+                      also participated in Tele2 Iot talks to promote Buddywise.
+                      During my second internship, I further developed my thesis
+                      work that I had done. It was a function that was developed
+                      for Buddywise. An anonymisation feature to create a
+                      Gaussian blur effect when a notification is sent to the
+                      client. With this feature, every image that the client
+                      receives remains anonymous and GDPR safe. <br />
+                      <i>You can find more info under projects </i>
                     </p>
-                  </div> */}
+                  </article>
+                  <article className="work mb-4">
+                    <h2>Work</h2>
+                    <h3>
+                      Internship as Front-end developer at Buddywise 2022-2023
+                    </h3>
+                    <p>
+                      Finalised the user interface and developed the anonymous
+                      feature.
+                    </p>
+                    <h3 className="mt-3 ">
+                      Store manager at Färgscalan 2010-2021
+                    </h3>
+                    <p>
+                      Responsible for the store's operation and onboarding for
+                      new employees.
+                    </p>
+                  </article>
+                </Col>
+                <Col>
+                  <TechStack />
                 </Col>
               </Row>
             </Container>
---
+++ src/utils/techStack.tsx
@@ -0,0 +1,146 @@
+import JavaScript from '../assets/icons/icons8-javascript.svg';
+import Angular from '../assets/icons/icons8-angular-48(1).png';
+import Bootstrap from '../assets/icons/icons8-bootstrap.svg';
+import CSS from '../assets/icons/icons8-css-48.png';
+import Figma from '../assets/icons/icons8-figma-64.png';
+import Git from '../assets/icons/icons8-git-50.png';
+import Github from '../assets/icons/icons8-github-48.png';
+import HTML from '../assets/icons/icons8-html-5-48.png';
+import MongoDB from '../assets/icons/icons8-mongo-db-64.png';
+import MySQL from '../assets/icons/icons8-my-sql-64.svg';
+import NodeJS from '../assets/icons/icons8-node-js(1).svg';
+import React from '../assets/icons/icons8-react-80.svg';
+import Sass from '../assets/icons/icons8-sass(1).svg';
+import TypeScript from '../assets/icons/icons8-typescript(1).svg';
+import WebAccessiblity from '../assets/icons/icons8-web-accessibility-50.png';
+import { Col } from 'react-bootstrap';
+function TechStack() {
+  return (
+    <Col md={8} lg={6} className=" offset-md-6 offset-lg-4 ">
+      <article>
+        <h2>Skills</h2>
+        <img
+          className="iconHover"
+          src={HTML}
+          alt="Html Icon"
+          width="50"
+          height="50"
+          title="Html"
+        />
+        <img
+          className="iconHover"
+          src={CSS}
+          alt="CSS Icon"
+          width="50"
+          height="50"
+          title="CSS"
+        />
+        <img
+          className="iconHover"
+          src={Sass}
+          alt="Sass Icon"
+          width="50"
+          height="50"
+          title="Sass"
+        />
+        <img
+          className="iconHover"
+          src={Bootstrap}
+          alt="Bootstrap Icon"
+          width="50"
+          height="50"
+          title="Bootstrap"
+        />
+        <img
+          className="iconHover"
+          src={JavaScript}
+          alt="JavaScript Icon"
+          width="50"
+          height="50"
+          title="JavaScript"
+        />
+        <img
+          className="iconHover"
+          src={React}
+          alt="React Icon"
+          width="50"
+          height="50"
+          title="React"
+        />
+        <img
+          className="iconHover"
+          src={Angular}
+          alt="Angular Icon"
+          width="50"
+          height="50"
+          title="Angular"
+        />
+        <img
+          className="iconHover"
+          src={TypeScript}
+          alt="TypeScript Icon"
+          width="50"
+          height="50"
+          title="TypeScript"
+        />
+        <img
+          className="iconHover"
+          src={Git}
+          alt="Git Icon"
+          width="50"
+          height="50"
+          title="Git"
+        />
+        <img
+          className="iconHover"
+          src={Github}
+          alt="Github Icon"
+          width="50"
+          height="50"
+          title="Github"
+        />
+        <img
+          className="iconHover"
+          src={MongoDB}
+          alt="MongoDB Icon"
+          width="50"
+          height="50"
+          title="MongoDB"
+        />
+        <img
+          className="iconHover"
+          src={MySQL}
+          alt="MySQL Icon"
+          width="50"
+          height="50"
+          title="MySQL"
+        />
+        <img
+          className="iconHover"
+          src={NodeJS}
+          alt="NodeJS Icon"
+          width="50"
+          height="50"
+          title="NodeJS"
+        />
+        <img
+          className="iconHover"
+          src={Figma}
+          alt="Figma Icon"
+          width="50"
+          height="50"
+          title="Figma"
+        />
+        <img
+          className="iconHover"
+          src={WebAccessiblity}
+          alt="WebbAccessibility Icon"
+          width="50"
+          height="50"
+          title="Web Accessibility"
+        />
+      </article>
+    </Col>
+  );
+}
+export default TechStack;
